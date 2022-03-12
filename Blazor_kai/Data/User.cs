a067a03986c491afa3743aabaeea6edbf5fc5ed6
AlmazDefourten <almaz.sahabiev@outlook.com>
2022-03-12 16:28:53 +0300

    files

---
+++ Blazor_kai/Data/User.cs
@@ -0,0 +1,22 @@
+﻿namespace kai_hack.Models
+{
+    public class User
+    {
+        public User(string name, string status, string email)
+        {
+            Name = name;
+            Status = status;
+            eMail = email;
+        }
+
+        public User()
+        {
+
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+
+        public string eMail { get; set; }
+    }
+}
